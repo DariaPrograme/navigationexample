24fe11471749a9812fa7acf441d993d1f8c0258f
DariaPrograme <dadidenko2006@icloud.com>
2021-09-26 13:59:01 +0300

    cat component

---
+++ cat.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+const Cat =()=>{
+    return(
+        <Text>Cat</Text>
+    )
+}
+export default Cat;
